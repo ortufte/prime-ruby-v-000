92870b82ad9153306bcf46d34414f3d6e9bfacf5
Olivia <ortufte@gmail.com>
2019-09-18 21:08:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(n)
   array = (2..n).to_a 
- if n<= 1
-   return false 
-  elsif array.include? {|i| i % n != 0} 
+  if n <= 1
+    return false array {|i| i % n != 0} 
     return true 
   end
 end
