a028bcb30eab5551523b5bceb938d55c28f2917c
Olivia <ortufte@gmail.com>
2019-09-18 21:17:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(n)
   array = (2...n).to_a 
   array.each do |i|
-     if i % n != 1 
-       return false 
+     if i % n == 1 
+       return true 
      else
        return true 
      end
