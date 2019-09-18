3209968abd04259ba2ad46ded4d6c9b3b54a251d
Olivia <ortufte@gmail.com>
2019-09-18 21:05:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
   array = (2..n).to_a 
  if n<= 1
    return false 
-  elsif array.any? {|i| i % n == 0} 
+  elsif array.any? {|i| i % n != 0} 
     return false
   else 
     return true 
